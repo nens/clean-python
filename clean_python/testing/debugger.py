fa7967b6c6e0d85a2be4a21d82e3a5727029566b
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2023-07-12 13:20:29 +0200

    Fix debugger

--- clean_python/testing/debugger.py
+++ clean_python/testing/debugger.py
@@ -7,13 +7,13 @@ def setup_debugger(*, host: str = "0.0.0.0", port: int = 5678):
     # Only to be used in development. Should someone inadvertently set DEBUG to True in
     # staging or production, a ModuleNotFoundError will be raised, because debugpy is
     # only available via requirements-dev.txt - this is intentionally.
+    if os.environ.get("DEBUG") or os.environ.get("DEBUG_WAIT_FOR_CLIENT"):
+        try:
+            import debugpy
 
-    try:
-        import debugpy
-
-        debugpy.listen((host, port))
-        if os.environ("DEBUG_WAIT_FOR_CLIENT"):
-            print("🔌 debugpy waiting for a client to attach 🔌", flush=True)
-            debugpy.wait_for_client()
-    except (ModuleNotFoundError, RuntimeError) as e:
-        print(e, flush=True)
+            debugpy.listen((host, port))
+            if os.environ.get("DEBUG_WAIT_FOR_CLIENT"):
+                print("🔌 debugpy waiting for a client to attach 🔌", flush=True)
+                debugpy.wait_for_client()
+        except (ModuleNotFoundError, RuntimeError) as e:
+            print(e, flush=True)
