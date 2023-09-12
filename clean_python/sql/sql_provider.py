f3c475376dffd607c38bb8b1dba9586a9b577091
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2023-09-12 14:47:43 +0200

    Fix typing

--- clean_python/sql/sql_provider.py
+++ clean_python/sql/sql_provider.py
@@ -6,6 +6,7 @@ from typing import AsyncIterator
 from typing import Dict
 from typing import List
 from typing import Optional
+from typing import Sequence
 
 from sqlalchemy import text
 from sqlalchemy.exc import DBAPIError
@@ -83,7 +84,7 @@ class SQLDatabase(SQLProvider):
     async def drop_database(self, name: str) -> None:
         await self._execute_autocommit(text(f"DROP DATABASE IF EXISTS {name}"))
 
-    async def truncate_tables(self, names: list[str]) -> None:
+    async def truncate_tables(self, names: Sequence[str]) -> None:
         quoted = [f'"{x}"' for x in names]
         await self._execute_autocommit(text(f"TRUNCATE TABLE {', '.join(quoted)}"))
 
