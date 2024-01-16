f41790e93d5355865740f6509cad41c9ff865f17
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2024-01-16 17:08:53 +0100

    Compare sql providers

--- clean_python/base/infrastructure/__init__.py
+++ clean_python/base/infrastructure/__init__.py
@@ -1,5 +1,6 @@
 from .in_memory_gateway import *  # NOQA
 from .internal_gateway import *  # NOQA
 from .mapper import *  # NOQA
+from .null_gateway import *  # NOQA
 from .tmpdir_provider import *  # NOQA
 from .typed_internal_gateway import *  # NOQA
