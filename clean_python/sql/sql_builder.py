f0b29fb54734b8f6f3051c09a10071423b9c0e59
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2024-02-15 16:43:12 +0100

    Actually comit the new file

---
+++ clean_python/sql/sql_builder.py
@@ -0,0 +1,57 @@
+from sqlalchemy import and_
+from sqlalchemy import delete
+from sqlalchemy import Executable
+from sqlalchemy import Table
+from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.sql.expression import ColumnElement
+from sqlalchemy.sql.expression import false
+
+from clean_python import ctx
+from clean_python import Filter
+from clean_python import Id
+from clean_python import Json
+
+
+class SQLBuilder:
+    def __init__(self, table: Table, multitenant: bool):
+        self.table = table
+        self.multitenant = multitenant
+
+    @property
+    def current_tenant(self) -> int | None:
+        if not self.multitenant:
+            return None
+        if ctx.tenant is None:
+            raise RuntimeError(f"{self.__class__} requires a tenant in the context")
+        return ctx.tenant.id
+
+    def _filter_to_sql(self, filter: Filter) -> ColumnElement:
+        try:
+            column = getattr(self.table.c, filter.field)
+        except AttributeError:
+            return false()
+        if len(filter.values) == 0:
+            return false()
+        elif len(filter.values) == 1:
+            return column == filter.values[0]
+        else:
+            return column.in_(filter.values)
+
+    def _filters_to_sql(self, filters: list[Filter]) -> ColumnElement:
+        qs = [self._filter_to_sql(x) for x in filters]
+        if self.multitenant:
+            qs.append(self.table.c.tenant == self.current_tenant)
+        return and_(*qs)
+
+    def _id_filter_to_sql(self, id: Id) -> ColumnElement:
+        return self._filters_to_sql([Filter(field="id", values=[id])])
+
+    def insert(self, item: Json) -> Executable:
+        return insert(self.table).values(**item).returning(self.table)
+
+    def delete(self, id: Id) -> Executable:
+        return (
+            delete(self.table)
+            .where(self._id_filter_to_sql(id))
+            .returning(self.table.c.id)
+        )
